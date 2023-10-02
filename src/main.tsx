71654aaf73deac093896654f0eba9a338d296226
Daniel Okoronkwo <danielokoronkwo90@gmail.com>
2023-10-02 15:48:17 +0100

    chore: effect environment variable setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./index.css";
+
 import { ThemeContextProvider, AuthContextProvider } from "./context";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ToastContainer } from "react-toastify";
